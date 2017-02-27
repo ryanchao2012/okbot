77d90bdd805d423e3e3b950a919c189290f37102
Ryan Chao <ryanchao2012@gmail.com>
2017-02-27 10:28:56 +0000

    postgresql setting

--- okbot/settings.py
+++ okbot/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'okbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['OKBOT_DB_NAME'],
+        'USER': os.environ['OKBOT_DB_USER'],
+        'PASSWORD': os.environ['OKBOT_DB_PASSWORD'],
+        'HOST': '',
+        'PORT': '',
     }
 }
 
