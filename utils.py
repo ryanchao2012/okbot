e9091a16554d5ce3c42fb1cfbb72a3db447e648f
Ryan Chao <ryanchao2012@gmail.com>
2017-04-15 09:30:52 +0000

    fix PsqlAbstract wrapper

--- utils.py
+++ utils.py
@@ -13,21 +13,28 @@ class PsqlAbstract(object):
         self.db = db or self.DB_NAME
         self.pw = password or self.DB_PASSWORD
 
-    def session(func):
-        def _wrapper(self, *args, **kwargs):
-            connect = psycopg2.connect(database=self.db, user=self.user, password=self.pw)
-            cursor = connect.cursor()
-            ret = func(self, cursor, **kwargs)
-            cursor.close()
-            connect.close()
-            return ret
+    @staticmethod
+    def session(keep=False):
+        def _session(func):
+            def _wrapper(self, *args, **kwargs):
+                connect = psycopg2.connect(database=self.db, user=self.user, password=self.pw)
+                cursor = connect.cursor()
+                ret = func(self, connect, cursor, **kwargs)
+                if not keep:
+                    PsqlAbstract._close(connect, cursor)
 
-        return _wrapper
+                return ret
+
+            return _wrapper
+        return _session
+
+    @staticmethod
+    def _close(connect, cursor):
+        cursor.close()
+        connect.close()
+
 
 class PsqlQuery(PsqlAbstract):
-    # OKBOT_DB_USER = os.environ['OKBOT_DB_USER']
-    # OKBOT_DB_NAME = os.environ['OKBOT_DB_NAME']
-    # OKBOT_DB_PASSWORD = os.environ['OKBOT_DB_PASSWORD']
 
     def __init__(self, username=None, db=None, password=None):
         super(self.__class__, self).__init__(username=username, db=db, password=password)
@@ -38,12 +45,10 @@ class PsqlQuery(PsqlAbstract):
             self._get_schema(query_=q, data=data)
         return self._query(query_=q, data=data)
 
-    @PsqlAbstract.session
-    def _get_schema(self, cursor, query_=None, data=None):
+    @PsqlAbstract.session()
+    def _get_schema(self, connect, cursor, query_=None, data=None):
         if query_ is None:
             return
-        # connect = psycopg2.connect(database=self.db, user=self.user, password=self.pw)
-        # cursor = connect.cursor()
         idx_semicln = query_.find(';')
         if idx_semicln > 0:
             query_ = query_[:idx_semicln]
@@ -51,19 +56,15 @@ class PsqlQuery(PsqlAbstract):
         cursor.execute(query_, data)
         schema = [desc[0] for desc in cursor.description]
         # print('Warning: schema changed:', schema)
-        # cursor.close()
-        # connect.close()
         self.schema = {k: v for v, k in enumerate(schema)}
 
-    @PsqlAbstract.session
-    def _query(self, cursor, query_=None, data=None):
+    @PsqlAbstract.session(keep=True)
+    def _query(self, connect, cursor, query_=None, data=None):
         if query_ is None:
             return
-        # connect = psycopg2.connect(database=self.db, user=self.user, password=self.pw)
-        # cursor = connect.cursor()
         cursor.execute(query_, data)
         for record in cursor:
             yield record
-        # cursor.close()
-        # connect.close()
+        PsqlAbstract._close(connect, cursor)
+
 
